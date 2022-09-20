77208dbabd3aacd81b997a584ee008bebcdecb8b
Dhiraj-JC <dhiraj@trainitsolutions.com>
2022-09-20 14:10:44 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mongo = require('mongodb').MongoClient;
+
 dotenv.config('./env')
 
 const app = express();
 app.use(express.json())
 app.use(cors())
-app.get('/',(req,res)=>{
-    res.send("hello world")
-})
+
+
+
+const url = "mongodb+srv://admin:%40@cluster0.2za6crn.mongodb.net/?retryWrites=true&w=majority";
+
+let db;
+let students;
+
+mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true},(error,client)=>{
+    if(error){
+        console.log(error);
+        return;
+    }
+    db = client.db('StudentDatabase');   
+    students = db.collection('Students');
+});
+
+app.post('/students',(req,res)=> {
+    let student = req.body;
+    students.insertOne(student,(error, result)=> {
+        if(error) {
+            console.error(error);
+            res.status(500).json({error: error});
+        } else {
+            res.status(201).json({result: result});
+        }
+    });
+});
+
+app.get('/students',(req,res)=> {
+    res.json({result: 'Hi everyone from heroku'})
+});
+
+app.get('/students/:studentId',(req,res)=> {
+
+});
+
+app.put('/students',(req,res)=> {
+
+});
+
+app.delete('/students/:studentId',(req,res)=> {
+
+});
+
+
+
 app.listen(process.env.PORT, () => {
     console.log('Server is listening on ' + process.env.PORT)
 })
